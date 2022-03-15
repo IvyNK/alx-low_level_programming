9926465db22438ab75e1bca7a1a5fae75eaa322c
root <ivykamau65@gmail.com>
2022-03-15 13:51:03 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	while (a < 123)
+	{
+		putchar(a);
+		a++;
+	}
+	while (b < 91)
+	{
+		putchar(b);
+		b++;
+	}
+return (0);
+}

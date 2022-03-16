4c131a60def0b75dadd2c8fd2e0a28789051037c
root <ivykamau65@gmail.com>
2022-03-16 04:04:44 -0700

    Numbers Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d\n", a);
+	}
+	return (0);
+}

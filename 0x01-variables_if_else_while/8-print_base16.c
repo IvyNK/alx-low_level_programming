d53461cd9c9a89b3eea6c4e861548c87ab85f3d5
root <ivykamau65@gmail.com>
2022-03-16 15:17:31 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 48;
+
+	while (a <= 57)
+	{
+		putchar(a);
+		a++;
+	}
+	int b = 65;
+
+	while (b <= 70)
+	{
+		putchar (b);
+		b++;
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
